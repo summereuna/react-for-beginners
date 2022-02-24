f8af52dfb9a6139d30bcea9a73a25298168623d9
summereuna <summereuna@gmail.com>
2022-02-24 22:32:37 +0900

    To Do List fix: item order

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     // 첫 번째 인자로 현재 state 받아오는 함수 적어주자.
     //그리고 새로운 array 받아와서 return해주자. 이 부분이 상당히 중요하다. state는 항상 새로운 거여야 한다.
     //그리고 그 새로운 []에는 방금 적은 toDo와, toDos를 넣어주면 된다.
-    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDos((currentArray) => [...currentArray, toDo]);
     //그리고 submit되고 나서는 input 값 비워주자. 그러려면 모디파이어에 빈 값 주면 된다.
     setToDo("");
   };
