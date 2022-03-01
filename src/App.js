d1c81fd22ef0fc2230c004449a583950a7cd2683
summereuna <summereuna@gmail.com>
2022-03-01 19:57:46 +0900

    Movie App: add Route basename

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/hey" element={<h1>BTS</h1>}></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/movie/:id"
+          element={<Detail />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
